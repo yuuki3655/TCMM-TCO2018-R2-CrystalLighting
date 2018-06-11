da17c26a425d11e1b1fcff43765513769f894201
Yuki Hamada <yuuki3655@gmail.com>
2018-06-10 23:23:12 -0700

    Update update_score.sh script and reduces parallel run to 4 from 8. Rerun submission 1 and update score

--- update_score.sh
+++ update_score.sh
@@ -5,14 +5,10 @@ make release.o CrystalLightingVis.class
 rm -rf scores
 mkdir scores
 
-./tools/update_score_worker.sh 1 100 &
-./tools/update_score_worker.sh 101 200 &
-./tools/update_score_worker.sh 201 300 &
-./tools/update_score_worker.sh 301 400 &
-./tools/update_score_worker.sh 401 500 &
-./tools/update_score_worker.sh 501 600 &
-./tools/update_score_worker.sh 601 700 &
-./tools/update_score_worker.sh 701 800 &
+./tools/update_score_worker.sh 1 200 &
+./tools/update_score_worker.sh 201 400 &
+./tools/update_score_worker.sh 401 600 &
+./tools/update_score_worker.sh 601 800 &
 wait
 
 awk '{s+=$3} END {printf "Total = %.1f\n", s}' scores/score_*.txt | tee score.txt
